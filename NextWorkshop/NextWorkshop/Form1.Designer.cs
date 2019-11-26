7a8e80bedabb9d11e748bf2576ec551eb8280ecd
Thomas Godon <thomas.godon@gmail.com>
2019-11-26 15:34:28 +0100

    Add template & DataCollection to caspar device

--- NextWorkshop/NextWorkshop/Form1.Designer.cs
+++ NextWorkshop/NextWorkshop/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtLijn1 = new System.Windows.Forms.TextBox();
+            this.txtLijn2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.cdColorPicker = new System.Windows.Forms.ColorDialog();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLijn1
+            // 
+            this.txtLijn1.Location = new System.Drawing.Point(90, 32);
+            this.txtLijn1.Name = "txtLijn1";
+            this.txtLijn1.Size = new System.Drawing.Size(426, 20);
+            this.txtLijn1.TabIndex = 0;
+            // 
+            // txtLijn2
+            // 
+            this.txtLijn2.Location = new System.Drawing.Point(90, 58);
+            this.txtLijn2.Name = "txtLijn2";
+            this.txtLijn2.Size = new System.Drawing.Size(426, 20);
+            this.txtLijn2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lijn 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lijn 2";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(74, 148);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "LOAD";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(155, 148);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblColor
+            // 
+            this.lblColor.BackColor = System.Drawing.Color.White;
+            this.lblColor.Location = new System.Drawing.Point(91, 85);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(32, 32);
+            this.lblColor.TabIndex = 6;
+            this.lblColor.Click += new System.EventHandler(this.lblColor_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Color";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(299, 148);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(540, 183);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLijn2);
+            this.Controls.Add(this.txtLijn1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtLijn1;
+        private System.Windows.Forms.TextBox txtLijn2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.ColorDialog cdColorPicker;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- NextWorkshop/NextWorkshop/Form1.cs
+++ NextWorkshop/NextWorkshop/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Svt.Caspar;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,72 @@ namespace NextWorkshop
 {
     public partial class Form1 : Form
     {
+        private CasparDevice _device = new CasparDevice();
+
         public Form1()
         {
             InitializeComponent();
+
+            // init caspar device
+            _device.Settings.Hostname = "172.20.130.69";
+            _device.Settings.Port = 5250;
+            _device.Settings.AutoConnect = true;
+
+            // set color
+            lblColor.BackColor = cdColorPicker.Color;
+
+            // connect to caspar server
+            _device.Connect();
+
+            // set connection status delegate
+            _device.ConnectionStatusChanged += new EventHandler<Svt.Network.ConnectionEventArgs>(delegate (object sender, Svt.Network.ConnectionEventArgs e) 
+            {
+                if (e.Connected)
+                {
+                    this.SetTitleBar($"Connected to {e.Hostname} at {e.Port}");
+                }
+                else
+                {
+                    this.SetTitleBar("Disconnected");
+                }
+            });
+        }
+
+        private void SetTitleBar(string Text)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                this.Text = Text;
+            });
+        }
+
+        private void lblColor_Click(object sender, EventArgs e)
+        {
+            cdColorPicker.ShowDialog();
+            lblColor.BackColor = cdColorPicker.Color;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            // create datacollection
+            CasparCGDataCollection DataCollection = new CasparCGDataCollection();
+            DataCollection.SetData("Lijn1", txtLijn1.Text);
+            DataCollection.SetData("Lijn2", txtLijn2.Text);
+            DataCollection.SetData("Color", $"0x{cdColorPicker.Color.R.ToString("X2")}{cdColorPicker.Color.G.ToString("X2")}{cdColorPicker.Color.B.ToString("X2")}");
+
+            // load template with datacollection
+            _device.Channels[0].CG.Add(0, "kdg/lowerthird", DataCollection);
+            _device.Channels[0].CG.Play(0);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            _device.Channels[0].CG.Next(0);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            _device.Channels[0].Clear();
         }
     }
 }
